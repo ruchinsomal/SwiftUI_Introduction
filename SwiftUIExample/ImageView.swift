cdd051c2bb061d859c029e73c25dcd30b7c8053d
Apple <apple@Apples-MacBook-Pro.local>
2020-01-12 22:26:22 +0530

    First Commit, MKMapView, TextView, ImageView with padding and shadow, Combining all view in a container vertical stack view (VStack)

---
+++ SwiftUIExample/ImageView.swift
@@ -0,0 +1,21 @@
+//
+//  ImageView.swift
+//  SwiftUIExample
+//
+//  Created by Apple on 11/01/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView()
+    }
+}
---
+++ SwiftUIExample/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  SwiftUIExample
+//
+//  Created by Apple on 12/01/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
